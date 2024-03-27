eea4f5e7386786a9ed33864606934af60777b73b
Anton Stremovskyy <stremovskyy@me.com>
2024-03-27 12:19:07 +0200

    module fix (test)

--- errors.go
+++ errors.go
@@ -3,3 +3,5 @@ package go_ipay
 import "errors"
 
 var ErrRequestIsNil = errors.New("request is nil")
+var ErrMerchantIsNil = errors.New("merchant is nil")
+var ErrPersonalDataIsNil = errors.New("personal data is nil")
