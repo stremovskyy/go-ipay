3f15c95e218094248df6df7351482d4e11876627
Anton Stremovskyy <stremovskyy@me.com>
2025-10-28 15:07:32 +0200

    feat: initialize Info in WithPersonalData if nil before merging

--- ipay/requets_constructor.go
+++ ipay/requets_constructor.go
@@ -1,25 +1,12 @@
 /*
- * MIT License
+ * Project: banker
+ * File: requets_constructor.go (10/28/25, 3:07 PM)
  *
- * Copyright (c) 2024 Anton Stremovskyy
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (C) Megakit Systems 2017-2025, Inc - All Rights Reserved
+ * @link https://www.megakit.pro
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Anton (antonstremovskyy) Stremovskyy <stremovskyy@gmail.com>
  */
 
 package ipay
@@ -243,6 +230,10 @@ func WithLanguage(language Lang) func(*RequestWrapper) {
 
 func WithPersonalData(data *Info) func(*RequestWrapper) {
 	return func(rw *RequestWrapper) {
+		if rw.Request.Body.Info == nil {
+			rw.Request.Body.Info = &Info{}
+		}
+
 		rw.Request.Body.Info.MergeWith(data)
 	}
 }
